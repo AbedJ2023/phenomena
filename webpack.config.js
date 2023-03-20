c2f3571e65bc93222927ddc36139ac4b819bf619
Abed <abedj2023@Gmail.com>
2023-03-19 19:36:59 -0500

    passed all the tests

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+"use strict";
+module.exports = {
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.css$/, // styles files
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
