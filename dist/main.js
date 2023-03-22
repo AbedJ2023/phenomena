9568d03e222b7130e8406b3416d4f47a244ff629
Abed <abedj2023@Gmail.com>
2023-03-21 21:24:55 -0500

    fixed error

--- dist/main.js
+++ dist/main.js
@@ -9810,65 +9810,6 @@ const App = () => {
     type: "submit"
   }, "Create Report")));
 };
-
-// const App = () => {
-//   const [reports, setReports] = useState([]);
-//   const [title, setTitle] = useState([""]);
-//   const [location, setLocation] = useState([""]);
-//   const [description, setDescription] = useState([""]);
-
-//   useEffect(() => {
-//     const getReports = async () => {
-//       const response = await axios.get("/api/reports");
-//       console.log(response.data);
-//     };
-//     getReports();
-//   });
-
-//   const onChange = (event) => {
-//     const name = event.target.name;
-//     const value = event.target.value;
-//     if (name === "title") {
-//       setTitle(value);
-//     } else if (name === "location") {
-//       setLocation(value);
-//     }
-//   };
-//   return (
-//     <>
-//       <h1>hello</h1>
-//       <ul>
-//         {reports.map((report) => {
-//           return <li>{report.title}</li>;
-//         })}
-//       </ul>
-//       <form>
-//         <input
-//           type="text"
-//           value={title}
-//           onChange={onChange}
-//           name="title"
-//           placeholder="title"
-//         />
-//         <input
-//           type="text"
-//           value={location}
-//           onChange={onChange}
-//           name="location"
-//           placeholder="location"
-//         />
-//         <input
-//           type="text"
-//           value={description}
-//           onChange={onChange}
-//           name="description"
-//           placeholder="description"
-//         />
-//       </form>
-//     </>
-//   );
-// };
-
 const root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_2__.createRoot)(document.getElementById("root"));
 root.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(App, null));
 })();
